89135498980c86df25aa049ef4849f86af9dc957
Sambit Mohanty <sambitmohanty1947@gmail.com>
2024-10-30 10:16:22 +0530

    resolving dependecy issues

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add any other configuration options you had in your next.config.ts
+};
+
+module.exports = nextConfig;
